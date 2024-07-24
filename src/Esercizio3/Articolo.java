8d15b2480d239a96e9ad69956dcb8399862de16b
Andrea <ageria20@gmail.com>
2024-07-24 17:00:24 +0200

    inizio es3

--- src/Esercizio3/Articolo.java
+++ src/Esercizio3/Articolo.java
@@ -1,4 +1,31 @@
 package Esercizio3;
 
+import java.util.Random;
+
 public class Articolo {
+
+    private int codArticle;
+    private String description;
+    private double price;
+    private int pieces;
+
+    public Articolo() {
+        Random random = new Random();
+        this.codArticle = random.nextInt(1, 100);
+        this.description = "";
+        this.price = 0;
+        this.pieces = 0;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setPieces(int pieces) {
+        this.pieces = pieces;
+    }
 }
